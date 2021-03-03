d5139f5bfda85764fe5d183b3d3c1f92cd36b2a7
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2021-03-03 15:33:33 +0600

    Update Lab 2 mirko -5 var.cpp

--- Lab 2 mirko -5 var/Lab 2 mirko -5 var/Lab 2 mirko -5 var.cpp
+++ Lab 2 mirko -5 var/Lab 2 mirko -5 var/Lab 2 mirko -5 var.cpp
@@ -1,4 +1,4 @@
-﻿// Lab 2 mirko -5 var.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Lab 2 mirko -5 var.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //вычисление логарифма
 
 #include <iostream>
@@ -7,25 +7,38 @@
 using namespace std;
 #define end 100
 
+
+double PositiveInt(string x) { // проверка на ввод положительного числа >=0
+    double t;
+    while (true)
+    {
+        cout << x;
+        cin >> t;
+        if (!cin || t <= -1 || t >= 1)
+        {
+            if (!cin)
+                cout << "Enter The Number!\n";
+            if (t <= -1 || t >= 1)
+                cout << "enter Number from -1 to 1!\n";
+
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+        else {
+            break;
+        }
+    }
+    return t;
+}
+
 int main()
 {
     double Result, delta, x;
     int n;
-    Result = 0; x = 10.0;
+    Result = 0;
     cout << "Enter x from -1 to 1 =  ";
-    while (x <= -1, x >= 1) //проверка ввода значений
-    {
-        
-        cin >> x;
-        if (x <= -1, x >= 1)
-        {
-            cout << "PLEACE enter x FROM -1 to 1 =  ";
-        }
-        
-    }
+    x = PositiveInt("Enter X: ");
     
-    cout << "you enter x = " << x; // вывод введенного x
-    cout << "\n";
     for (n = 0; n<= end; n++) //расчет логарифма
     {
         delta = (pow(-1,n) * pow(x,(2*n+1))) / (2*n+1);
@@ -39,6 +52,3 @@ int main()
 
 
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
