1122813d9f0c56f82c2fbb2c2953a917fc86687d
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2021-03-25 20:24:09 +0600

    Add files via upload

---
+++ lab 4.5.cpp
@@ -0,0 +1,45 @@
+﻿// lab 4.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int EnterCheck(string x) { 
+    int n = 0;
+    while (true)
+    {
+        cout << x;
+        cin >> n;
+        if (!cin)
+        {
+            cout << "Введите еще раз\n";
+            cin.clear();
+        }
+        else break;
+    }
+    return n;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x = EnterCheck("Введите x: ");
+    int end = EnterCheck("Введите границу ряда: ");
+
+    double y = 0;
+    int factMaxNumber = 2, degree = 2;
+    for (int n = 0; n < end; n++) {
+        double fact = 1;
+            for (int n = 1; n <= factMaxNumber; n++) {
+                fact = fact * n;
+            }
+            factMaxNumber = factMaxNumber + 2;
+        y = y + (pow(x, degree) / fact);
+        degree = degree + 2;
+    }
+    double res = y + 1;
+    cout << "\n" << "Гиперболический косинус x с границей ряда " << end << " = " << res;
+    
+    return 0;
+}
