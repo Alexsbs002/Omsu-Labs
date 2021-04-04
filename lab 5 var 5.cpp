4b4151307631d78f3d11dcaeef1c8094fa2bf4de
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2021-04-04 11:48:27 +0600

    Add files via upload

---
+++ lab 5 var 5.cpp
@@ -0,0 +1,22 @@
+﻿// lab 4.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//обратный порядок строки
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    
+
+    string str;
+    cout << "Enter the string: ";
+    getline(cin, str);//чтение строки из потока
+
+    reverse(str.begin(), str.end());
+    //меняет порядок элементов в заданном диапозоне, можно писать begin->begin()+2
+    cout << str << endl;
+    
+    return 0;
+}
